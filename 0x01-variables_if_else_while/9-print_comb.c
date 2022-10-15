1e84520800811a973c30617c4419030830d2be0d
Bartholomew Obiya <Barthobiya@live.com>
2022-10-15 11:52:56 -0700

    ppp

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = '0'; k <= '9'; k++)
+	{
+		putchar(k);
+		if (k == 9)
+		{
+			break;
+		}
+		else
+		{
+			putchar(',');
+			putchar('');
+		}
+	}
+		putchar('\n');
+		return (0);
+}
