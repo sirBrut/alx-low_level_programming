9fbafa7fd9b9c20ad826213306e05faca9f1b3ad
Bartholomew Obiya <Barthobiya@live.com>
2022-10-19 09:36:25 -0700

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,8 @@
 #define main_h
 extern int _putchar(char c);
 extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+#endif
