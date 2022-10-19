71f2333f0808c55158808fe7dc6df416d475dfe5
Bartholomew Obiya <Barthobiya@live.com>
2022-10-19 09:27:06 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,8 +8,14 @@
 
 int main(void)
 {
-	int i;
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
 
-	printf("_putchar");
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#ifndef main_h
+#define main_h
+extern int _putchar(char c);
+extern void print_alphabet(void);
